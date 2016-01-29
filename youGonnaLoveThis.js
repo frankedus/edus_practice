970acac8f58d0b25b79047cb9de1e9c20029c437
Balint Bejczi <bejczi.balint@gmail.com>
2016-01-29 23:29:10 +0100

    add weekend exercise

---
+++ youGonnaLoveThis.js
@@ -0,0 +1,21 @@
+//make a method that takes a number and return true, if the number is a palindrome
+// feel free to use, the previous written python algorithm
+// hint1: you can compare numbers as strings, by number convert to string => String(3)='3'
+
+
+
+
+
+
+
+//console.log(isPalindrome(121)) => true
+//console.log(isPalindrome(23)) => false
+
+
+//if then previous one was to easy for you, pleas implement a method, that returns the prime numbers between 2 and 100. If the current number is a palindrome too, than return 'Zeno' instead of the number.
+//feel free to use, the previous written pythn algorithm
+//test method is missing on purpose.
+
+
+
+//Good Luck! :)
