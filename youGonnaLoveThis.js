b0793a4a18f69496e3ff0b086c0d421dfb510df2
Edina Frank <frankedus@gmail.com>
2016-01-31 22:09:38 +0100

    practice

--- youGonnaLoveThis.js
+++ youGonnaLoveThis.js
@@ -2,9 +2,19 @@
 // feel free to use, the previous written python algorithm
 // hint1: you can compare numbers as strings, by number convert to string => String(3)='3'
 
+function isPalindrome(number) {
+  var palindrome = '';
+  for (var i = (String(number).length) - 1; i >=0; i--) {
+    palindrome += String(number)[i];
+  }
+  if (palindrome === String(number)) {
+    return true
+  } else {
+    return false
+  }
+}
 
-
-
+console.log(isPalindrome(12233221));
 
 
 
